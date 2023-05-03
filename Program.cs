07874fc660e6b1cb81428f99e7dbda09e6c21db5
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-05-03 09:27:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igra_na_karte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zbroj = 0, broj;
+            while(zbroj < 31)
+            {
+                Console.WriteLine("Upisi te vrijednost karte: ");
+                broj = Convert.ToInt32(Console.ReadLine());
+                if(broj >=1 && broj <= 13)
+                {
+                    zbroj += broj;
+                }
+            }
+            if(zbroj == 31)
+            {
+                Console.WriteLine("Pobjedili ste!");
+            }
+            else
+            {
+                Console.WriteLine("Izgubili ste");
+            }
+            Console.ReadKey();
+        }
+    }
+}
